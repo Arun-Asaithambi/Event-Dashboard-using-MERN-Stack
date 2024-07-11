1ae04f3fadc53e0cde5439837daa6831cc07224c
Arun-Asaithambi <167624082+Arun-Asaithambi@users.noreply.github.com>
2024-07-11 21:04:13 +0530

    feat: add vite in client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>react app</h1>
+    </>
+  )
+}
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
 const express = require ("express");
 const app = express();
+const mongoose = require('mongoose');
+const eventRoutes = require ("./routes/eventsRoutes")
 const cors = require ('cors')
 
 app.use(express.json())
 app.use(cors())
 
-const mongoose = require('mongoose');
+app.use("/events", eventRoutes)
+
+
 main().catch(e=> console.log(e.message));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/dashboard');
   console.log('connected to db');
 }
-
-const eventRoutes = require ("./routes/eventsRoutes")
-
-app.use("/events", eventRoutes)
-
-
 
 app.listen(3000, () =>{
     console.log("server is running on server 3000");
